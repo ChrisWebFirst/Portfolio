0bdd7ddb17b7b68e15db1965e82a34079fedd57f
chrisb <chrisb@web1st.design>
2022-06-17 14:16:41 -0500

    update

---
+++ assets/js/hobbies.js
@@ -0,0 +1,13 @@
+﻿
+import json from '../data/hobbies.json' assert {type: 'json'};
+
+const { createApp } = Vue
+createApp({     
+        data() {
+        return {
+            header: "Hobbies",
+            detail:"A little bit about the hobbies I enjoy",
+            hobby: json,
+            };
+        },    
+  }).mount('#app')
